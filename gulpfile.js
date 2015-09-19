4d816b695f0123a9f3e8799918cb1ef81dd9d0a0
SecondHong <whpigpig@gmail.com>
2015-09-20 02:13:45 +0800

    Setting up gulp sass and nodemon --save-dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'),
+    livereload = require('gulp-livereload'),
+    sass = require('gulp-sass');
 
 gulp.task('default', function() {
-  // place code for your default task here
+
+    livereload.listen();
+
+    nodemon({
+
+        script: '',
+        ext: 'js html jade',
+        env: { 'NODE_ENV': 'development' }
+
+    }).on('restart', function () {
+
+        console.log('Restarted Server.');
+
+        gulp.src('.').pipe(livereload());
+
+    });
+
+    gulp.watch('./scss/**/*.scss', ['sass']);
+
+});
+
+gulp.task('sass', function () {
+    gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/stylesheets')).pipe(livereload());
 });
